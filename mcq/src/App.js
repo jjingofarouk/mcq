704703cbec4ac3f3be4bb5fc8ea4ba34220cceaf
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-15 12:49:28 -0700

    Update App.js

--- mcq/src/App.js
+++ mcq/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home'; // Import the new Home component
 import Quiz from './components/Quiz';
 import Results from './components/Results';
 import SpecialtySelector from './components/SpecialtySelector';
@@ -9,17 +10,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="header">
-          <h1>Welcome to the Medical MCQ Quiz</h1>
-          <p>Your go-to app for practicing medical questions across specialties.</p>
-        </header>
-
-        <section className="cta">
-          <h2>Start Practicing</h2>
-          <Link to="/specialty" className="cta-btn">Select Specialty</Link>
-        </section>
-
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/specialty" element={<SpecialtySelector />} />
           <Route path="/quiz/:specialtyId/:questionId" element={<Quiz />} />
           <Route path="/results" element={<Results />} />
