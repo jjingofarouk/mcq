57c93a7bf69781ee17671a6419e29d6beec20cb9
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-14 17:08:21 -0700

    Update App.js

--- mcq/src/App.js
+++ mcq/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Quiz from './components/Quiz';
+import Results from './components/Results';
+import SpecialtySelector from './components/SpecialtySelector';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Medical MCQ Quiz</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/specialty" element={<SpecialtySelector />} />
+          <Route path="/quiz/:specialtyId/:questionId" element={<Quiz />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
