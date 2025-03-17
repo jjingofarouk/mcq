959b3ca14b5b6ff78b3c98b38112893590d946af
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-17 02:20:47 -0700

    Update App.js

--- mcq/src/App.js
+++ mcq/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home'; // Import the new Home component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SpecialtySelector from './components/SpecialtySelector';
 import Quiz from './components/Quiz';
 import Results from './components/Results';
-import SpecialtySelector from './components/SpecialtySelector';
 import './App.css';
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/specialty" element={<SpecialtySelector />} />
-          <Route path="/quiz/:specialtyId/:questionId" element={<Quiz />} />
+          <Route path="/specialties" element={<SpecialtySelector />} />
+          <Route path="/quiz/:specialtyId" element={<Quiz />} />
           <Route path="/results" element={<Results />} />
         </Routes>
       </div>
