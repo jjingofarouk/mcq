92c9fdf5a981cd42a9068e81a5ea67965c13da22
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-13 11:02:35 +0300

    Update App.js

--- mcq/src/App.js
+++ mcq/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Quiz from './components/Quiz';
@@ -8,14 +10,26 @@ import Results from './components/Results';
 import './App.css';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <Navbar onSidebarToggle={toggleSidebar} />
+        <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quiz/:specialtyId" element={<Quiz />} />
           <Route path="/results" element={<Results />} />
+          <Route path="/quizzes" element={<Home />} />
+          <Route path="/progress" element={<Home />} />
+          <Route path="/leaderboard" element={<Home />} />
+          <Route path="/profile" element={<Home />} />
+          <Route path="/logout" element={<Home />} />
         </Routes>
         <Footer />
       </div>
